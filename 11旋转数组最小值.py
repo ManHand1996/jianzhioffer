e7a4535d9a9c016b60c00a3ece934b5a487bff5a
manhand <manhand1996@gmail.com>
2023-04-30 16:49:48 +0800

    11旋转数组最小值

---
+++ 11旋转数组最小值.py
@@ -0,0 +1,64 @@
+"""
+把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+
+给你一个可能存在 重复 元素值的数组 numbers ，它原来是一个升序排列的数组，
+并按上述情形进行了一次旋转。请返回旋转数组的最小元素。
+例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为 1。  
+
+注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+
+ 
+
+示例 1：
+
+输入：numbers = [3,4,5,1,2]
+输出：1
+示例 2：
+
+输入：numbers = [2,2,2,0,1]
+输出：0
+ 
+
+提示：
+
+n == numbers.length
+1 <= n <= 5000
+-5000 <= numbers[i] <= 5000
+numbers 原来是一个升序排序的数组，并进行了 1 至 n 次旋转
+"""
+
+def minArray(self, numbers):
+    """
+    :type numbers: List[int]
+    :rtype: int
+    """
+    # 遍历所有
+    # min = numbers[0]
+    # for i in numbers:
+    #     if min >= i:
+    #         min = i
+    # return min
+    """
+    最优解:二分法
+    设m为中间下标, i,j 分别为数组的头尾下标
+    由于输入数组是升序排序数组经过旋转(最开始的若干个元素移动到数组最后位置)后的结果
+    即:输入数组为最多两个有序的子数组合并
+    所以有:
+    m = (i + j)/2
+    numbers[m] > numbers[j]: 旋转点(最小值)在m的右边 [m+1, j]
+    numbers[m] < numbers[j]: 旋转点(最小值)在m的左边 [i,m]
+    numbers[m] == numbers[j]: j = j -1 往前移动一位
+    退出条件: i == j
+    i ,j = 0, len(numbers)-1
+    """
+    i, j = 0, len(numbers) - 1
+    while i != j:
+        mid = (i + j)/2
+        if numbers[mid] > numbers[j]:
+            i = mid + 1
+        elif numbers[mid] < numbers[j]:
+            j = mid
+        else:
+            j -= 1
+
+    return numbers[i]
